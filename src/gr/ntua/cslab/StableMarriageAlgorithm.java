774de30421000e076d26e6d58e84a6441a074822
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2013-06-05 18:24:38 +0300

    wrote loop for StableMarriageAlgorithm

--- src/gr/ntua/cslab/StableMarriageAlgorithm.java
+++ src/gr/ntua/cslab/StableMarriageAlgorithm.java
@@ -1,5 +1,28 @@
 package gr.ntua.cslab;
 
+import java.util.Iterator;
+
+/**
+ * This class implements the classic algorithm which solves the Stable Marriage Problem proposed by Shapley and Roth.
+ * The algorithm is given two sets of preferences (corresponding to male and female preferences) and estimates a stable 
+ * matching.
+ * @author Giannis Giannakopoulos
+ *
+ */
 public class StableMarriageAlgorithm {
 
+	private PersonList men, women;
+	
+	public StableMarriageAlgorithm(PersonList men, PersonList women) {
+		this.men=men;
+		this.women=women;
+	}
+	
+	public void step(){
+		Iterator<Person> it = this.men.getFreePersonIterator();
+		while(it.hasNext()){
+			Person man = it.next(), woman =this.women.get(man.getPreferences().getNextPreference()); 
+			man.propose(woman);
+		}
+	}
 }
