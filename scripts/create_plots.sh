e049b4dbc9a6fc9b96c5b4709e740033d26543d1
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2013-06-14 11:42:01 +0300

    update scripts

--- scripts/create_plots.sh
+++ scripts/create_plots.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # script used to create plots given a dataset
 
+PICS_DIR="pics/"
+mkdir $PICS_DIR
+
 PLOT_NAME[2]='Number of steps'
 PLOT_NAME[3]='Number of marriages'
 PLOT_NAME[4]='Men average rank'
 PLOT_NAME[5]='Women average rank'
 PLOT_NAME[6]='Couple average rank'
 
+PLOT_SCRIPT=`find . -name plot_one.sh`
 for i in `seq 2 6` ; do
-	PLOT_TITLE=${PLOT_NAME[$i]};
-	scripts/plot_one.sh $i
-	`pwd`
+	export PLOT_TITLE=${PLOT_NAME[$i]};
+	$PLOT_SCRIPT $i $PICS_DIR
 done
+
+# step used to pack every image to a single pdf file 
+# comment it out if you need the separate images
+
+convert $PICS_DIR* $PICS_DIR/graphs_`date "+%y%m%d_%H%M%S"`.pdf
--- scripts/plot_one.sh
+++ scripts/plot_one.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-
 #script used to create a single plot image
+# first argument is necessary and it point to the column that will be depicted
+# second argument is used to point to the output path
+
 if [ $# -lt 1 ]; then
 	echo "Expect number of column to print";
 	exit;
 fi
+if [ -z "$OUTPUT_FILE" ]; then
+	OUTPUT_FILE=`find ../ -name current`
+fi
+
+PICTURE_PATH="$2pic$1.png";
 
 SEC_COL=$[6+$1];
 
-GNUPLOT_COMMAND="set grid;";
+GNUPLOT_COMMAND="set grid; set terminal png size 1024,768; set output '$PICTURE_PATH';";
+
 
 if [ -z "$PLOT_TITLE" ]; then
 	GNUPLOT_COMMAND=$GNUPLOT_COMMAND;
 else
-	GNUPLOT_COMMAND=$GNUPLOT_COMMAND"set title  '$PLOT_TITLE';";
+	GNUPLOT_COMMAND=$GNUPLOT_COMMAND"set title  '$PLOT_TITLE vs Dataset size'; set xlabel 'Dataset size'; set ylabel '$PLOT_TITLE'; ";
 fi
 
-GNUPLOT_COMMAND=$GNUPLOT_COMMAND"plot 	'output/current' using 1:$1 with lines title 'SMA',\
-			'output/current' using 1:$SEC_COL with lines title 'FSMA'";
+
+GNUPLOT_COMMAND=$GNUPLOT_COMMAND"plot 	'$OUTPUT_FILE' using 1:$1 with lines title 'SMA',\
+			'$OUTPUT_FILE' using 1:$SEC_COL with lines title 'FSMA'";
 gnuplot -p -e  "$GNUPLOT_COMMAND";
+
+
