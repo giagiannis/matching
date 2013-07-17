df63aa8a63983c5813f365d05f12985f583b6ca6
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2013-07-17 12:18:18 +0300

    added Preferences interface and renamed in memory preferences to MemoryPreferences (also repackaged)

---
+++ src/gr/ntua/cslab/hama/containers/preferences/Preferences.java
@@ -0,0 +1,12 @@
+package gr.ntua.cslab.hama.containers.preferences;
+
+public interface Preferences {
+
+	public int getNext();
+	
+	public boolean hasMore();
+	
+	public int getNextRank();
+	
+	public int getRank(int id);
+}
