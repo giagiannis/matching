ab9ab42a779daad989dbf8d9039e35b6eda2d679
Giannis Giannakopoulos <giannisgianna@gmail.com>
2013-08-24 13:55:40 +0300

    added PrunedPreferences

---
+++ src/gr/ntua/cslab/containers/preferences/PrunedPreferences.java
@@ -0,0 +1,67 @@
+package gr.ntua.cslab.containers.preferences;
+
+import java.util.TreeSet;
+
+/**
+ * Class used to keep the "next" preference. This class consists of a container which keeps ranks and not id's of next person.
+ * @author giannis
+ *
+ */
+public class PrunedPreferences {
+
+	private TreeSet<Integer> prefereneces;
+	private int size;
+	
+	public PrunedPreferences(int size) {
+		this.prefereneces = new TreeSet<Integer>();
+		this.prefereneces.add(1);
+		this.size = size;
+	}
+	
+	/**
+	 * Adds an element to the list.
+	 * @param rank
+	 * @return
+	 */
+	public boolean addPreference(int rank){
+		if(rank > this.prefereneces.last())
+			return false;
+		if(rank == this.prefereneces.last()-1)
+			this.prefereneces.remove(this.prefereneces.last());
+		this.prefereneces.add(rank);
+		return true;
+	}
+	
+	/**
+	 * Returns the next preference. 
+	 * @return
+	 */
+	public int getNext(){
+		Integer head = this.prefereneces.first();
+		this.prefereneces.remove(head);
+		if(this.prefereneces.size()==0 && (head+1<=this.size)){
+			this.prefereneces.add(head+1);
+		}
+		return head;
+	}
+	
+	/**
+	 * True if the preference list contains more values, else false.
+	 * @return
+	 */
+	public boolean hasMorePreferences(){
+		return !this.prefereneces.isEmpty();
+	}
+	
+	public static void main(String[] args) {
+		PrunedPreferences pref = new PrunedPreferences(10);
+		for(int i=0;i<8;i++)System.out.println(pref.getNext());
+		pref.addPreference(2);
+		
+		pref.addPreference(5);
+		System.out.println(pref.getNext());
+		System.out.println(pref.getNext());
+		System.out.println(pref.getNext());
+	}
+	
+}
